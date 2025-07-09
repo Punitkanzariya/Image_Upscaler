a6c10d464517509d5a08860f8b63e734e46ee9ca
Punit Kanzariya <punitkanzariya4706@gmail.com>
2025-07-09 11:37:24 +0530

    update

--- app.py
+++ app.py
@@ -108,10 +108,7 @@ def index():
     return render_template("index.html")
 
 # Run app
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
 
